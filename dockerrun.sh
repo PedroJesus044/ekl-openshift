55f7c57b91e204e2d02ec970a3322cc97478416d
Pedro Jesús Ibarra Palmero <pedro_jesus_ip@live.com.mx>
2024-07-16 11:55:20 -0600

    Elasticsearch

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,3 +1,6 @@
 #/bin/bash
 docker build --tag 'elastic-ubuntu24' .
-docker run -it 'elastic-ubuntu24' bash
+docker rm -f elasticsearch
+docker run -p 9200:9200 -dit --rm --name elasticsearch 'elastic-ubuntu24'
+
+docker ps
